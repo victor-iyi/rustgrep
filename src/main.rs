f15c614653442e1c86766b748681625f99efcbfd
Victor I. Afolabi <javafolabi@gmail.com>
2019-08-17 14:29:14 +0100

    Renamed package form minigrep to rustgrep

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     // Parse command line arguments.
-    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+    let config = rustgrep::Config::new(&args).unwrap_or_else(|err| {
         eprintln!("{}", err);
         eprintln!("Usage: {} `needle` `haystack.txt`", args[0]);
         std::process::exit(1);
     });
 
     // Start the program.
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = rustgrep::run(config) {
         eprintln!("Application Error: {}", e);
         std::process::exit(1);
     }
